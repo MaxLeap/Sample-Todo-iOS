25fcafe2d1c423ef8884883b93bc638b3c18677e
Jin Sun <jsun@ilegendsoft.com>
2015-06-29 18:29:05 +0800

    initialize

---
+++ LASTodoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LASTodoList
+//
+//  Created by Sun Jin on 6/9/15.
+//  Copyright (c) 2015 ilegendsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
